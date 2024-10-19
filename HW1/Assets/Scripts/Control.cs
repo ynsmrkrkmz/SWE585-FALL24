35a39221f5d9673f6d9a4c5907a8a2f914e7634c
Yunus Emre Korkmaz <ynsmrkrkmaz@gmail.com>
2024-10-19 23:52:12 +0300

    feat[HW1]: player control script modified and camera follows player func added

--- HW1/Assets/Scripts/Control.cs
+++ HW1/Assets/Scripts/Control.cs
@@ -5,22 +5,24 @@ using UnityEngine;
 public class Control : MonoBehaviour
 {
     public float moveSpeed = 5f;
-    Rigidbody m_Rigidbody;
+    public float mouseSensitivity = 100f;
+    private float xRotation = 0f;
     // Start is called before the first frame update
     void Start()
     {
-        m_Rigidbody = gameObject.GetComponent<Rigidbody>();
+        Cursor.lockState = CursorLockMode.Locked;
     }
 
     void Update()
     {
         float moveHorizontal = Input.GetAxis("Horizontal");
         float moveVertical = Input.GetAxis("Vertical"); 
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
 
-        // Create a movement vector based on input
         Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+        transform.Translate(moveSpeed * Time.deltaTime * movement);
 
-        // Apply movement to the Rigidbody
-        m_Rigidbody.AddForce(movement * moveSpeed);
+        transform.Rotate(Vector3.up * mouseX);
     }
 }
